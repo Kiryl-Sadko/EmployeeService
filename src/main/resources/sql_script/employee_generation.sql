441437d19a13b678ef5d104873a5b1df9e329b32
Kiryl Sadko <kirill.beltech@gmail.com>
2021-06-11 19:04:29 +0300

    dataBase generation script has created

---
+++ src/main/resources/sql_script/employee_generation.sql
@@ -0,0 +1,23 @@
+do
+$$
+    DECLARE
+        i integer := 0;
+    begin
+        WHILE i < 5
+            loop
+                INSERT INTO public.employee(first_name,
+                                            last_name,
+                                            department_id,
+                                            job_title,
+                                            gender,
+                                            date_of_birth)
+                VALUES (concat('first_name_', i),
+                        concat('last_name', i),
+                        i,
+                        concat('developer_', i),
+                        'female',
+                        '1995-08-03');
+                i := i + 1;
+            END loop;
+    end
+$$;
