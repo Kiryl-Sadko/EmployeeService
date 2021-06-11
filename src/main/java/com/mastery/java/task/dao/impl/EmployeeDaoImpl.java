2a0ef1ed95ef1a35c063d83da231a42b2a98ed47
Kiryl Sadko <kirill.beltech@gmail.com>
2021-06-11 19:03:37 +0300

    EmployeeDao has fixed

--- src/main/java/com/mastery/java/task/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/com/mastery/java/task/dao/impl/EmployeeDaoImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
-    
+
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Employee> rowMapper;
 
